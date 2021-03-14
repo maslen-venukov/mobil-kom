610075088bf4b55f3201f42a7363f4c1ccea0d6a
Sergei <maslenv@bk.ru>
2021-03-14 12:33:00 +0500

    toggle checkboxes in sidebar

--- js/secret.js
+++ js/secret.js
@@ -1,14 +1,17 @@
 const sideSecret = document.querySelector('.side .secret')
 const resultSecret = document.querySelector('.result .secret')
 const secretToggle = document.querySelector('#secret')
+const secretList = document.querySelector('.secret__list')
+const secretSaveBtn = document.querySelector('.secret__save')
 const secretCheckboxes = document.querySelectorAll('.secret__item .secret__checkbox')
 const secretFields = document.querySelectorAll('.secret__field')
 const sercretTextareas = document.querySelectorAll('.secret__textarea')
 
 secretToggle.addEventListener('change', () => {
+  const elems = [secretList, resultSecret, secretSaveBtn]
   secretToggle.checked
-    ? resultSecret.style.display = 'block'
-    : resultSecret.style.display = 'none'
+    ? elems.forEach(el => el.style.display = 'block')
+    : elems.forEach(el => el.style.display = 'none')
 })
 
 secretCheckboxes.forEach((checkbox, index) => {
