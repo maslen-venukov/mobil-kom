86408d2914019869108efc2fea14c52260e13d06
Sergei <maslenv@bk.ru>
2021-03-14 11:17:49 +0500

    change text in result block

--- js/secret.js
+++ js/secret.js
@@ -3,6 +3,7 @@ const resultSecret = document.querySelector('.result .secret')
 const secretToggle = document.querySelector('#secret')
 const secretCheckboxes = document.querySelectorAll('.secret__item .secret__checkbox')
 const secretFields = document.querySelectorAll('.secret__field')
+const sercretTextareas = document.querySelectorAll('.secret__textarea')
 
 secretToggle.addEventListener('change', () => {
   secretToggle.checked
@@ -15,5 +16,17 @@ secretCheckboxes.forEach((checkbox, index) => {
     checkbox.checked
       ? secretFields[index].style.display = 'block'
       : secretFields[index].style.display = 'none'
+  })
+})
+
+sercretTextareas.forEach((textarea, index) => {
+  const title = secretFields[index].querySelector('.question__title')
+  title.textContent = textarea.value
+})
+
+sercretTextareas.forEach((textarea, index) => {
+  textarea.addEventListener('input', () => {
+    const title = secretFields[index].querySelector('.question__title')
+    title.textContent = textarea.value
   })
 })
